226df9de9dc57806ebe86c50b7ba2feb6e41735e
kyvinh <kyvinh@gmail.com>
2021-11-19 16:12:17 +0100

    add prisma to the NodeJS global type if in development

--- components/util/prismaClient.tsx
+++ components/util/prismaClient.tsx
@@ -1,5 +1,16 @@
 import { PrismaClient } from '@prisma/client'
 
-let prisma = new PrismaClient()
+declare global {
+    namespace NodeJS {
+        interface Global {
+            prisma: PrismaClient;
+        }
+    }
+}
+
+const prisma = global.prisma || new PrismaClient()
+
+// add prisma to the NodeJS global type if in development
+if (process.env.NODE_ENV === 'development') global.prisma = prisma
 
 export default prisma
