f2f1a4330e1e7b0c729a0d857eceabace32fa271
kyvinh <kyvinh@gmail.com>
2021-11-19 16:16:08 +0100

    show results

--- components/QuestionResults.tsx
+++ components/QuestionResults.tsx
@@ -1,4 +1,12 @@
 export const QuestionResults = ({question, results}) => {
-    console.log(results)
-    return <></>
+    // console.log(results)
+    return <>
+        {
+            results.map((answerResult, i) => {
+                return <div key={i}>
+                    <p>{answerResult.answerText}: {answerResult._count}</p>
+                </div>
+            })
+        }
+    </>
 }
