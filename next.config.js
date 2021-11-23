8e60aad0feb09c20e98d3bb54a9ef1c332ec96fa
kyvinh <kyvinh@gmail.com>
2021-11-23 12:04:59 +0100

    base config for nextjs i18n

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 module.exports = {
     poweredByHeader: false,
     productionBrowserSourceMaps: true,
+    reactStrictMode: true,
+    i18n: {
+        locales: ['fr', 'nl', 'en'],
+        defaultLocale: 'fr',
+    }
 }
