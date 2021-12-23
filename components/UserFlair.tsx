4f8ec7b58755e4219b2a4368abb4192690fed8fb
kyvinh <kyvinh@gmail.com>
2021-12-23 15:35:42 +0100

    UI user flair

--- components/UserFlair.tsx
+++ components/UserFlair.tsx
@@ -5,7 +5,7 @@ export const UserFlair = ({user, theme="outline-dark"}) => {
         return null
     }
 
-    return <Button size="sm" variant={theme}>
+    return <Button size="sm" variant={theme} className="user-flair">
         { user.name ? user.name : "Un voisin" }
         <Badge pill className="mx-1" bg="secondary">{ user.reputationPoints}</Badge>
         <span className="visually-hidden">reputation points</span>
