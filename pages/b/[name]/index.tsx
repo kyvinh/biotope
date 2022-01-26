eb929acf1595f31faedb00480b6c350b9180da9e
kyvinh <kyvinh@gmail.com>
2022-01-26 17:19:16 +0100

    style biotope question template

--- pages/b/[name]/index.tsx
+++ pages/b/[name]/index.tsx
@@ -46,6 +46,67 @@ export default function BiotopeHome() {
                 </div>
             </section>
 
+            <section className="question-area">
+                <div className="container">
+                    <div className="question-main-bar pt-40px pb-40px">
+                        <div className="questions-snippet border-top border-top-gray">
+
+                            {b.questions && b.questions.map((question) => {
+
+                                const maxShortDescriptionLength = 200;
+                                question.shortDescription = question.description.length < maxShortDescriptionLength ? question.description : question.description.substring(0, maxShortDescriptionLength) + '…'
+
+                                return <div key={question.id} className="d-inline-flex media media-card rounded-0 shadow-none mb-0 bg-transparent p-2 border-bottom border-bottom-gray">
+                                    <div className="votes text-center votes-2">
+                                        <div className="vote-block">
+                                            <span className="vote-counts d-block text-center pr-0 lh-20 fw-medium">{question.votes}</span>
+                                            <span className="vote-text d-block fs-13 lh-18">votes</span>
+                                        </div>
+                                        <div className="answer-block answered my-2">
+                                            <span className="answer-counts d-block lh-20 fw-medium">?</span>
+                                            <span className="answer-text d-block fs-13 lh-18">answers</span>
+                                        </div>
+                                        <div className="view-block">
+                                            <span className="view-counts d-block lh-20 fw-medium">?</span>
+                                            <span className="view-text d-block fs-13 lh-18">views</span>
+                                        </div>
+                                    </div>
+                                    <div className="media-body">
+                                        <h5 className="mb-2 fw-medium">
+                                            <Link href={`/b/${b.name}/q/${question.id}`}><a>{question.name}</a></Link>
+                                        </h5>
+                                        <p className="mb-2 truncate lh-20 fs-15">{question.shortDescription}</p>
+                                        <div className="tags">
+                                            {question.tags.map((tag) => <a href="#" className="tag-link">{tag.name}</a>)}
+                                        </div>
+                                        <div className="media media-card user-media align-items-center px-0 border-bottom-0 pb-0">
+                                            <a href="user-profile.html" className="media-img d-block">
+                                                <img src="images/img3.jpg" alt="avatar" />
+                                            </a>
+                                            <div className="media-body d-flex flex-wrap align-items-center justify-content-between">
+                                                <div>
+                                                    <h5 className="pb-1"><a href="user-profile.html">Arden Smith</a></h5>
+                                                    <div className="stats fs-12 d-flex align-items-center lh-18">
+                                                        <span className="text-black pr-2" title="Reputation score">224</span>
+                                                        <span className="pr-2 d-inline-flex align-items-center" title="Gold badge"><span className="ball gold"/>16</span>
+                                                        <span className="pr-2 d-inline-flex align-items-center" title="Silver badge"><span className="ball silver"/>93</span>
+                                                        <span className="pr-2 d-inline-flex align-items-center" title="Bronze badge"><span className="ball"/>136</span>
+                                                    </div>
+                                                </div>
+                                                <small className="meta d-block text-right">
+                                                    <span className="text-black d-block lh-18">asked</span>
+                                                    <span className="d-block lh-18 fs-12">6 hours ago</span>
+                                                </small>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            })}
+                        </div>
+                    </div>
+                </div>
+            </section>
+
             <div className="biotope-container">
 
                 {session?.user?.isAnon &&
