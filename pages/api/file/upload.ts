b5b0ddf2b2314eecb1471483b123e285e8ad2bbe
kyvinh <kyvinh@gmail.com>
2022-02-02 21:59:44 +0100

    first step to file upload

--- pages/api/file/upload.ts
+++ pages/api/file/upload.ts
@@ -1,13 +1,25 @@
 import {HasUserIdAuthGuard} from "../../../lib/serverAnnotations";
-import {createHandler, Post} from "@storyofams/next-api-decorators";
+import {Body, createHandler, Post, Query, Req, UploadedFile, UploadedFiles} from "@storyofams/next-api-decorators";
+
+/*
+https://stackoverflow.com/questions/62411430/post-multipart-form-data-to-serverless-next-js-api-running-on-vercel-now-sh
+Try multer? https://stackoverflow.com/a/68882562
+
+// disable next.js' default body parser
+export const config = {
+    api: { bodyParser: false }
+}
+*/
 
 @HasUserIdAuthGuard()
 class ImageUploadHandler {
     @Post()
-    async create(/*@Body() body, @UploadedFile() singleFile, @UploadedFiles() files, @Query('userId') userId: string*/) {
-        // console.log('body', body)
-        // console.log('single', singleFile)
-        // console.log('files', files)
+    async create(@Body() body, @UploadedFile() singleFile, @UploadedFiles() files, @Query('userId') userId: string, @Req() req) {
+
+        console.log('body', body)
+        console.log('single', singleFile)
+        console.log('files', files)
+
         return {}
     }
 }
--- pages/b/[name]/edit.tsx
+++ pages/b/[name]/edit.tsx
@@ -1,18 +1,24 @@
-import {useForm} from "react-hook-form";
-import {fetcher} from "../../../components/util/fetcher";
-
 export default function BiotopeEditPage() {
 
-    const { register, handleSubmit } = useForm();
-
-    const onSubmit = async (data) => {
-        console.log(data);
-        const res = await fetcher(`/api/file/upload`, data);
-        console.log(res)
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        const fd = new FormData();
+        fd.append("logoImage", e.target.logoImage.files[0]);
+        const res = await fetch(window.location.origin + '/api/file/upload', {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+                "Content-Type": "multipart/form-data",
+                "Accept": "application/json",
+                "type": "formData",
+            },
+            body: fd
+        });
+        console.log('file upload result', res)
     };
 
-    return <form onSubmit={handleSubmit(onSubmit)}>
-        <input {...register("mainLogo")} type="file" />
+    return <form encType="multipart/form-data" onSubmit={onSubmit}>
+        <input name="logoImage" type="file" />
         <button>Submit</button>
     </form>
 }
