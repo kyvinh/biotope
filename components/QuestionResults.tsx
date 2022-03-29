78ce33e13f7cb972fabe064e671ce350504dd995
kyvinh <kyvinh@gmail.com>
2022-03-29 13:51:15 +0200

    show results details and incomplete answers

--- components/QuestionResults.tsx
+++ components/QuestionResults.tsx
@@ -11,9 +11,14 @@ export const computeResults = (possibleAnswers: PossibleAnswerWithArguments[], r
     const resultsWithCount: PossibleAnswerWithArgumentsAndCount[] = rawResults.reduce(
         (acc, rawResult) => {
             const possibleAnswer: PossibleAnswerWithArguments = possibleAnswers.find(element => element.id === rawResult.id)
-            const answerResult:PossibleAnswerWithArgumentsAndCount = {count: 0, percent: undefined, sameUserVotes: rawResult.sameUserVotes, votersCount: rawResult.votersCount, ...possibleAnswer}
-            answerResult.count = rawResult._count.answers
-            answerResult.percent = Number((answerResult.count / answerResult.votersCount * 100).toFixed(0))
+            const answerResult:PossibleAnswerWithArgumentsAndCount = {
+                count: rawResult._count.answers,
+                percent: Number((rawResult._count.answers / rawResult.votersCount * 100).toFixed(0)),
+                incomplete: rawResult.votersCount < maxVotersCount / 5,
+                sameUserVotes: rawResult.sameUserVotes,
+                votersCount: rawResult.votersCount,
+                ...possibleAnswer
+            }
             return [...acc, answerResult]
         }
         , [])
@@ -27,10 +32,12 @@ export const QuestionResults = ({question, results: rawResults, onArgumentUpdate
         return null
     }
 
-    const {totalVotesCount, resultsWithCount: answersWithCount, maxVotersCount} = computeResults(question.possibleAnswers, rawResults)
-    answersWithCount.sort((n1, n2) => n2.percent - n1.percent)
-
     const argumentsCount = question.possibleAnswers.reduce((acc, answer) => acc + answer.arguments.length, 0)
+    const {totalVotesCount, resultsWithCount: answersWithCount, maxVotersCount} = computeResults(question.possibleAnswers, rawResults)
+    answersWithCount.sort((n1, n2) => n1.incomplete ? 1 : n2.percent - n1.percent)
+
+    const [showDetails, setShowDetails] = useState(false)
+
     const onArgumentAdded = (possibleAnswerId, argument) => {
         const possibleAnswer = question.possibleAnswers.find(element => element.id === possibleAnswerId)
         possibleAnswer.arguments.push(argument)
@@ -51,18 +58,33 @@ export const QuestionResults = ({question, results: rawResults, onArgumentUpdate
     return <>
         <div className="subheader results-subheader">
             <div className="subheader-title">
-                <h3 className="fs-16">{messages.results.header}: {maxVotersCount} {maxVotersCount > 1 ? messages.results.respondents : messages.results.respondent} ({totalVotesCount} {totalVotesCount > 1 ? messages.results.votes : messages.results.vote})</h3>
+                <div className="d-flex align-items-center justify-content-between">
+                    <h3 className="fs-16">{messages.results.header}: {maxVotersCount} {maxVotersCount > 1 ? messages.results.respondents : messages.results.respondent} ({totalVotesCount} {totalVotesCount > 1 ? messages.results.votes : messages.results.vote})</h3>
+                    <a className="btn btn-outline-secondary" onClick={() => setShowDetails(!showDetails)}>{showDetails ? messages.results["details-hide"] : messages.results["details-show"]}</a>
+                </div>
             </div>
         </div>
         <div className="container-fluid mt-3 ps-0 pe-0">
+            {showDetails &&
+                <div className="text-end"><small>{messages.results["details-legend"]}</small></div>
+            }
             {answersWithCount.map(answerResult => {
                 return <div className="row mb-2">
                     <div className="col-5">
                         <h5 className="text-end border-bottom border-bottom-gray">{answerResult.possibleText ? answerResult.possibleText : answerResult.possibleNumber}</h5>
                     </div>
                     <div className="col d-flex align-items-center">
-                        <ProgressBar className="flex-fill" variant={`${answerResult.order + 1}`} now={answerResult.percent} label={`${answerResult.percent}%`} />
+                        {answerResult.incomplete ?
+                            <ProgressBar className="flex-fill progress-incomplete" variant={`none`} now={100} label={messages.results.incomplete}/>
+                            :
+                            <ProgressBar className="flex-fill" variant={`${answerResult.order + 1}`} now={answerResult.percent} label={`${answerResult.percent}%`}/>
+                        }
                     </div>
+                    {showDetails &&
+                        <div className="col-1 d-flex align-items-center">
+                            <small>{answerResult.count} / {answerResult.votersCount}</small>
+                        </div>
+                    }
                 </div>
             })}
         </div>
